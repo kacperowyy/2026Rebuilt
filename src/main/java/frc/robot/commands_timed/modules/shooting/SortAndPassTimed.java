1c065159d398e8e342ff38a036c612f0514d7331
francistudor <141586112+francistudor@users.noreply.github.com>
2026-02-24 18:03:17 +0100

    Add command for timed sort and pass

---
+++ src/main/java/frc/robot/commands_timed/modules/shooting/SortAndPassTimed.java
@@ -0,0 +1,39 @@
+package frc.robot.commands_timed.modules.shooting;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.Shooting;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SortAndPassTimed extends Command {
+    private final Shooting shooting;
+    private final Timer timer = new Timer();
+    private final double duration;
+
+    public SortAndPassTimed(Shooting shooting, double durationSeconds) {
+        this.shooting = shooting;
+        this.duration = durationSeconds;
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute() {
+        shooting.sortAndPass();   
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooting.stop();
+        timer.stop();
+    } 
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(duration);
+    }
+
+}
